f0930a3fa708a6f32407ec21667a02846ada8611
Melyssa Moya <120610288+dntfindmel@users.noreply.github.com>
2023-03-10 23:31:31 -0300

    Unfinished project

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+const ratings = document.querySelectorAll('.rating-item');
+let formValue = null
+
+function send() {
+    if(!formValue){
+        return window.alert('You must choose an option.')
+    } else {
+        window.open('validate.html')
+    }
+
+
+}
+
+function unclick() {
+    const selected = document.querySelectorAll('.rating-item.active');
+    for(var rating of ratings){
+        rating.classList.remove('active')
+    }
+}
+
+function select(event) {
+    const button = event.target
+    const value = button.innerText;
+
+    if (formValue == value) {
+        formValue = null
+    } else {
+        unclick()
+        formValue = value
+    }
+
+    button.classList.toggle('active')
+    console.log(formValue)
+}
+
+for(var rating of ratings){
+    rating.addEventListener('click', select)
+}
